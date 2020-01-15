f56c1492dca29172ac143a54914d494061ae67e7
Peter <pc.dean@icloud.com>
2020-01-15 23:40:25 +0000

    improved user input

--- directory.rb
+++ directory.rb
@@ -1,5 +1,3 @@
-=begin
-comment origonal code so i can implement user imput method
 # putting students in array
 students = [
   {name: 'Dr. Hannibal Lecter', cohort: :november},
@@ -14,10 +12,12 @@ students = [
   {name: 'Joffrey Baratheon', cohort: :november},
   {name: 'Norman Bates', cohort: :november},
 ]
+
 # method for header
 def print_header
   puts "The students of Villains Academy\n---------"
 end
+
 # method to print students
 def print(names)
   names.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" }
@@ -28,30 +28,18 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
-# calling print_header, print and print_footer
-print_header
-print students
-print_footer students
-=end
-
-# define students array
-students = []
-
-# define user input method
+# method to input students
 def input_students(students)
-  puts 'Please enter the names of the students'
+  puts "Please enter the names of the students you would like to add\nTo finish, just hit return twice"
   name = gets.chomp
   if name != ''
-    students << name
+    students << {name: name, cohort: :unkown}
     input_students(students)
-  else
-    print(students)
   end
 end
 
-# define print method
-def print(students)
-  students.each { |student| puts "#{student}" }
-end
-
+# calling input_students, print_header, print and print_footer
 input_students(students)
+print_header
+print students
+print_footer students
