12f4cfcc0a443aea807f307d8be4554cde24a3cc
Peter <pc.dean@icloud.com>
2020-01-15 22:58:35 +0000

    update student array to include cohort

--- directory.rb
+++ directory.rb
@@ -1,16 +1,16 @@
 # putting students in array
 students = [
-  'Dr. Hannibal Lecter',
-  'Darth Vader',
-  'Nurse Ratched',
-  'Michael Corleone',
-  'Alex DeLarge',
-  'The Wicked Witch of the West',
-  'Terminator',
-  'Freddy Krueger',
-  'The Joker',
-  'Joffrey Baratheon',
-  'Norman Bates',
+  ['Dr. Hannibal Lecter', :november],
+  ['Darth Vader', :november],
+  ['Nurse Ratched', :november],
+  ['Michael Corleone', :november],
+  ['Alex DeLarge', :november],
+  ['The Wicked Witch of the West', :november],
+  ['Terminator', :november],
+  ['Freddy Krueger', :november],
+  ['The Joker', :november],
+  ['Joffrey Baratheon', :november],
+  ['Norman Bates', :november],
 ]
 # method for header
 def print_header
@@ -18,7 +18,7 @@ def print_header
 end
 # method to print students
 def print(names)
-  names.each { |name| puts name}
+  names.each { |name| puts "#{name[0]} #{name[1]} cohort" }
 end
 
 # method to print number of students
