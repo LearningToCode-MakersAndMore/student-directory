a41ee925cfe1862cef1e11d35d7d4f7c7af57163
Peter <pc.dean@icloud.com>
2020-01-16 00:28:55 +0000

    added each students hobby new students default to painting

--- directory_improved.rb
+++ directory_improved.rb
@@ -1,16 +1,16 @@
 # putting students in array
 students = [
-  {name: 'Dr. Hannibal Lecter', cohort: :november},
-  {name: 'Darth Vader', cohort: :november},
-  {name: 'Nurse Ratched', cohort: :november},
-  {name: 'Michael Corleone', cohort: :november},
-  {name: 'Alex DeLarge', cohort: :november},
-  {name: 'The Wicked Witch of the West', cohort: :november},
-  {name: 'Terminator', cohort: :november},
-  {name: 'Freddy Krueger', cohort: :november},
-  {name: 'The Joker', cohort: :november},
-  {name: 'Joffrey Baratheon', cohort: :november},
-  {name: 'Norman Bates', cohort: :november},
+  {name: 'Dr. Hannibal Lecter', cohort: :november, hobby: 'eating people'},
+  {name: 'Darth Vader', cohort: :november, hobby: 'sewing'},
+  {name: 'Nurse Ratched', cohort: :november, hobby: 'being angry'},
+  {name: 'Michael Corleone', cohort: :november, hobby: 'having godchildren'},
+  {name: 'Alex DeLarge', cohort: :november, hobby: 'horology'},
+  {name: 'The Wicked Witch of the West', cohort: :november, hobby: 'scaring Alice'},
+  {name: 'Terminator', cohort: :november, hobby: 'time travel'},
+  {name: 'Freddy Krueger', cohort: :november, hobby: 'crime'},
+  {name: 'The Joker', cohort: :november, hobby: 'playing with batman'},
+  {name: 'Joffrey Baratheon', cohort: :november, hobby: 'nobody knows'},
+  {name: 'Norman Bates', cohort: :november, hobby: 'interior design'},
 ]
 
 # method for header
@@ -24,12 +24,14 @@ def print(names)
   # names.each_with_index { |student, i| puts "#{i+1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name][0].downcase == 't' }
   # bellow is commited code for printing names < 12 characters including spaces
   # names.each_with_index { |student, i| puts "#{i+1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].length < 12 }
-  students = names
-  count = 0
-  while count < students.count
-    puts "#{count+1}. #{students[count][:name]} (#{students[count][:cohort]} cohort)"
-    count += 1
-  end
+  # code for swaping each to while loop
+  # students = names
+  # count = 0
+  # while count < students.count
+  #  puts "#{count+1}. #{students[count][:name]} (#{students[count][:cohort]} cohort)"
+  #  count += 1
+  # end
+  names.each_with_index { |student, i| puts "#{i+1}. #{student[:name]} (#{student[:cohort]} cohort) hobby is: #{student[:hobby]}" }
 end
 
 # method to print number of students
@@ -42,7 +44,7 @@ def input_students(students)
   puts "Please enter the names of the students you would like to add\nTo finish, just hit return twice"
   name = gets.chomp
   if !name.empty?
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: :november, hobby: 'painting'}
     puts "Now we have #{students.count} students"
     input_students(students)
   end
